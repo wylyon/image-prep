c5cd758ce79f61ac00a2d7f2028e8c7d70aa92a4
Will Lyon <willis.lyon@yahoo.com>
2025-12-19 19:13:49 +0000

    work with canvas

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import ReactCrop, {
 
 import { canvasPreview } from "./canvasPreview";
 import { useDebounceEffect } from "./useDebounceEffect";
-import sharp from "sharp";
 
 import "react-image-crop/dist/ReactCrop.css";
 // This is to demonstate how to make and center a % aspect crop
@@ -58,7 +57,97 @@ export default function App() {
     }
   }
 
-  async function handleBlurClick() {}
+  async function handleSharpClick() {
+    const image = imgRef.current;
+    const previewCanvas = previewCanvasRef.current;
+    if (!image || !previewCanvas || !completedCrop) {
+      throw new Error("Crop canvas does not exist");
+    }
+
+    // This will size relative to the uploaded image
+    // size. If you want to size according to what they
+    // are looking at on screen, remove scaleX + scaleY
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+
+    const offscreen = new OffscreenCanvas(
+      completedCrop.width * scaleX,
+      completedCrop.height * scaleY
+    );
+    const ctx = offscreen.getContext("2d");
+    if (!ctx) {
+      throw new Error("No 2d context");
+    }
+    ctx.imageSmoothingEnabled = false;
+    ctx.drawImage(
+      previewCanvas,
+      0,
+      0,
+      previewCanvas.width,
+      previewCanvas.height,
+      0,
+      0,
+      offscreen.width,
+      offscreen.height
+    );
+    // You might want { type: "image/jpeg", quality: <0 to 1> } to
+    // reduce image size
+    const blob = await offscreen.convertToBlob({
+      type: "image/png",
+    });
+
+    if (blobUrlRef.current) {
+      URL.revokeObjectURL(blobUrlRef.current);
+    }
+    blobUrlRef.current = URL.createObjectURL(blob);
+    setImgSrc(blobUrlRef.current);
+  }
+
+  async function handleBlurClick() {
+    const image = imgRef.current;
+    const previewCanvas = previewCanvasRef.current;
+    if (!image || !previewCanvas || !completedCrop) {
+      throw new Error("Crop canvas does not exist");
+    }
+
+    // This will size relative to the uploaded image
+    // size. If you want to size according to what they
+    // are looking at on screen, remove scaleX + scaleY
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+
+    const offscreen = new OffscreenCanvas(
+      completedCrop.width * scaleX,
+      completedCrop.height * scaleY
+    );
+    const ctx = offscreen.getContext("2d");
+    if (!ctx) {
+      throw new Error("No 2d context");
+    }
+    ctx.filter = `blur(5px)`;
+    ctx.drawImage(
+      previewCanvas,
+      0,
+      0,
+      previewCanvas.width,
+      previewCanvas.height,
+      0,
+      0,
+      offscreen.width,
+      offscreen.height
+    );
+    // You might want { type: "image/jpeg", quality: <0 to 1> } to
+    // reduce image size
+    const blob = await offscreen.convertToBlob({
+      type: "image/png",
+    });
+
+    if (blobUrlRef.current) {
+      URL.revokeObjectURL(blobUrlRef.current);
+    }
+    blobUrlRef.current = URL.createObjectURL(blob);
+    setImgSrc(blobUrlRef.current);
+  }
 
   async function handleUpdateImageClick() {
     const image = imgRef.current;
@@ -220,6 +309,7 @@ export default function App() {
         </div>
         <div>
           <button onClick={handleBlurClick}>Blur Image</button>
+          <button onClick={handleSharpClick}>Sharppen Image</button>
         </div>
         <div>
           <button onClick={handleUpdateImageClick}>Update Image</button>
